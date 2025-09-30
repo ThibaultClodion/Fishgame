b8f2921170452a857ecea0564a82b18958f740eb
djpadbit <djpadbit@gmail.com>
2025-09-30 17:43:13 +0200

    Add FishData & FishType
    Signed-off-by: djpadbit <djpadbit@gmail.com>

--- Assets/Scripts/Fish/FishData.cs
+++ Assets/Scripts/Fish/FishData.cs
@@ -1,4 +1,20 @@
+using UnityEngine;
+
 public class FishData
 {
-    
+	public string Name {get; private set;}
+	public float Difficulty {get; private set;}
+	public int ExcludedMinigames {get; private set;}
+	public Sprite Sprite {get; private set;}
+	public float Longueur {get; private set;}
+	public float Largeur {get; private set;}
+
+	public FishData(string name, float diff, int excludedMG, Sprite spr, float longueur, float larg) {
+		this.Name = name;
+		this.Difficulty = diff;
+		this.ExcludedMinigames = excludedMG;
+		this.Sprite = spr;
+		this.Longueur = longueur;
+		this.Largeur = larg;
+	}
 }
--- Assets/Scripts/Fish/FishType.cs
+++ Assets/Scripts/Fish/FishType.cs
@@ -3,5 +3,23 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "FishType", menuName = "Scriptable Objects/FishType")]
 public class FishType : ScriptableObject
 {
-	
+	[SerializeField]
+	private string Name;
+	[SerializeField]
+	private Vector2 Difficulty;
+	[SerializeField]
+	private int ExcludedMinigames;
+	[SerializeField]
+	private Sprite Sprite;
+	[SerializeField]
+	private Vector2 Longueur;
+	[SerializeField]
+	private Vector2 Largeur;
+
+	public FishData GenerateFish() {
+		float diff = Random.Range(Difficulty.x, Difficulty.y);
+		float longueur = Random.Range(Longueur.x, Longueur.y);
+		float larg = Random.Range(Largeur.x, Largeur.y);
+		return new FishData(Name, diff, ExcludedMinigames, Sprite, longueur, larg);
+	}
 }
