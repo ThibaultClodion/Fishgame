06337f2d3b6af196714bd7a8d396457b738ff499
Thibault Clodion <thibaultclodion@gmail.com>
2025-10-14 10:47:26 +0200

    Add difficulty to ZoneMiniGame

--- Assets/Scripts/Minigames/ZoneMiniGame.cs
+++ Assets/Scripts/Minigames/ZoneMiniGame.cs
@@ -5,14 +5,29 @@ using UnityEngine.UI;
 public class ZoneMiniGame : BaseMiniGame
 {
     [Header("Zone Settings")]
-    [SerializeField] private float zonePortion = 0.1f;
-    [SerializeField] private float zoneMoveSpeed = 0.5f;
+    [SerializeField] private float minZonePortion = 0.15f;
+    [SerializeField] private float maxZonePortion = 0.15f;
+
+    [Header("Zone Speed Settings")]
+    [SerializeField] private float zoneMoveSpeed = 300f;
+    [SerializeField] private float gravity = 600f;
+    [SerializeField] private float maxFallSpeed = 400f;
+    private float currentFallSpeed = 0f;
 
     [Header("Progression Settings")]
-    [SerializeField] private float decreaseRate = 0.1f;
-    [SerializeField] private float increaseRate = 0.2f;
+    [SerializeField] private float minDecreaseRate = 0.1f;
+    [SerializeField] private float maxDecreaseRate = 0.15f;
+    private float decreaseRate;
+    [SerializeField] private float minIncreaseRate = 0.2f;
+    [SerializeField] private float maxIncreaseRate = 0.2f;
+    private float increaseRate;
 
+    [Header("Fish Settings")]
+    [SerializeField] private float minFishSpeed = 0.3f;
+    [SerializeField] private float maxFishSpeed = 0.5f;
+    private float fishSpeed;
 
+    [Header("References")]
     [SerializeField] private Slider fishSlider;
     [SerializeField] private RectTransform zoneTransform;
 
@@ -28,14 +43,15 @@ public class ZoneMiniGame : BaseMiniGame
     {
         base.Initialize(data);
 
-        // TODO : Adjust settings based on fish data (difficulty ...)
-
         // Initialize variables
         startTime = Time.time;
         sliderHeight = fishSlider.GetComponent<RectTransform>().rect.height;
-        zoneHeight = sliderHeight * zonePortion;
+        zoneHeight = sliderHeight * Mathf.Lerp(minZonePortion, maxZonePortion, data.Difficulty);
         minZonePos = zoneHeight / 2;
         maxZonePos = sliderHeight - zoneHeight / 2;
+        decreaseRate = Mathf.Lerp(minDecreaseRate, maxDecreaseRate, data.Difficulty);
+        increaseRate = Mathf.Lerp(minIncreaseRate, maxIncreaseRate, data.Difficulty);
+        fishSpeed = Mathf.Lerp(minFishSpeed, maxFishSpeed, data.Difficulty);
 
         // Set zone size and position
         zoneTransform.sizeDelta = new Vector2(zoneTransform.sizeDelta.x, zoneHeight);
@@ -47,6 +63,7 @@ public class ZoneMiniGame : BaseMiniGame
 
     public override void Disable()
     {
+        isHoldingButton = false;
         InputManager.Instance.SouthButtonAction.started -= HoldButton;
         InputManager.Instance.SouthButtonAction.canceled -= ReleaseButton;
     }
@@ -61,26 +78,30 @@ public class ZoneMiniGame : BaseMiniGame
     private void FishMovement()
     {
         // TODO : Improve fish IA movement
-        fishSlider.value = Mathf.PingPong((Time.time - startTime) * 0.5f, 1f);
+        fishSlider.value = Mathf.PingPong((Time.time - startTime) * fishSpeed, 1f);
     }
 
     private void ZoneMovement()
     {
-        // TODO : Improve movement for a more natural feeling (gravity ...)
+        float newY;
+
         if (isHoldingButton)
         {
-            zoneTransform.anchoredPosition = new Vector2(zoneTransform.anchoredPosition.x, zoneTransform.anchoredPosition.y + zoneMoveSpeed * Time.deltaTime);
+            currentFallSpeed = 0f;
 
-            if (zoneTransform.anchoredPosition.y > maxZonePos)
-                zoneTransform.anchoredPosition = new Vector2(zoneTransform.anchoredPosition.x, maxZonePos);
+            newY = Mathf.Min(zoneTransform.anchoredPosition.y + zoneMoveSpeed * Time.deltaTime, maxZonePos);
+
         }
         else
         {
-            zoneTransform.anchoredPosition = new Vector2(zoneTransform.anchoredPosition.x, zoneTransform.anchoredPosition.y - zoneMoveSpeed * Time.deltaTime);
+            // Gravity Effect
+            currentFallSpeed += gravity * Time.deltaTime;
+            currentFallSpeed = Mathf.Min(currentFallSpeed, maxFallSpeed);
 
-            if (zoneTransform.anchoredPosition.y < minZonePos)
-                zoneTransform.anchoredPosition = new Vector2(zoneTransform.anchoredPosition.x, minZonePos);
+            newY = Mathf.Max(zoneTransform.anchoredPosition.y - currentFallSpeed * Time.deltaTime, minZonePos);
         }
+
+        zoneTransform.anchoredPosition = new Vector2(zoneTransform.anchoredPosition.x, newY);
     }
 
     private void Progression()
