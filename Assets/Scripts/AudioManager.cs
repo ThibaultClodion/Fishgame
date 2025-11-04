f08fe2fa234a03cca585bba0a04a4dc16d00cf57
Thibault Clodion <thibaultclodion@gmail.com>
2025-11-04 16:47:10 +0100

    Add main music

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,53 @@
+using NUnit.Framework;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager Instance { get; private set; }
+
+    [SerializeField] private List<AudioClip> musics;
+    [SerializeField] private AudioSource musicSource;
+    [SerializeField] private AudioSource sfxSource;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else if (Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+    }
+
+    private void Start()
+    {
+        StartMusicLoop();
+    }
+
+    private void StartMusicLoop()
+    {
+        int musicIndex = Random.Range(0, musics.Count);
+        musicSource.clip = musics[musicIndex];
+        musicSource.loop = true;
+        musicSource.Play();
+    }
+
+    public void SetMusicVolume(float volume)
+    {
+        musicSource.volume = volume;
+    }
+
+    public void PlaySFX(AudioClip clip)
+    {
+        sfxSource.PlayOneShot(clip);
+    }
+
+    public void SetSFXVolume(float volume)
+    {
+        sfxSource.volume = volume;
+    }
+}
