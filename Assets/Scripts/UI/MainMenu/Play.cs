858e59a9ddd6bf4a6ee1e3b97242706b186cce54
djpadbit <djpadbit@gmail.com>
2025-11-14 09:34:53 +0100

    Select color change in menu + when switch to gamepad, select play.
    Signed-off-by: djpadbit <djpadbit@gmail.com>

--- Assets/Scripts/UI/MainMenu/Play.cs
+++ Assets/Scripts/UI/MainMenu/Play.cs
@@ -7,10 +7,21 @@ public class Play : MonoBehaviour
 {
     [SerializeField] private Button button;
 
+    private void InputModeChange(bool usingKeyboard) {
+        if (!usingKeyboard)
+            button.Select();
+    }
+
     private void OnEnable()
     {
+        InputManager.Instance.OnInputModeChangeEvent += InputModeChange;
         button.Select();
         button.onClick.AddListener(PlayGame);
+    }
+
+    private void OnDisable()
+    {
+        InputManager.Instance.OnInputModeChangeEvent -= InputModeChange;
     }
 
     private void PlayGame()
